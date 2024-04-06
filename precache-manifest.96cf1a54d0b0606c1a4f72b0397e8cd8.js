23effd28332ade172309180027393a3d3c48c529
chigivera <ayman.benchamkha@gmail.com>
2024-04-06 19:22:51 +0200

    Updates

---
+++ precache-manifest.96cf1a54d0b0606c1a4f72b0397e8cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ffe208eab1b7ba2a2a6478560d8edc",
+    "url": "/react-drum-machine/index.html"
+  },
+  {
+    "revision": "3530dbc5462f8947fb03",
+    "url": "/react-drum-machine/static/css/main.f82e497b.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/react-drum-machine/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/react-drum-machine/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "3530dbc5462f8947fb03",
+    "url": "/react-drum-machine/static/js/main.57f16265.chunk.js"
+  },
+  {
+    "revision": "db79261ec3f36331b395",
+    "url": "/react-drum-machine/static/js/runtime~main.aa62c9f2.js"
+  }
+]);
